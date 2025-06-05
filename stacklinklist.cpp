08c0a439e74d7f6d967b29f2becb97d6fd746343
Pillow <m.farid.ft20@mail.umy.ac.id>
2025-06-05 14:43:32 +0700

    first Commit

---
+++ stacklinklist.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
+
+class Stack
+{
+private:
+    Node *top;
+
+public:
+    Stack()
+    {
+        top = NULL;
+    }
+};

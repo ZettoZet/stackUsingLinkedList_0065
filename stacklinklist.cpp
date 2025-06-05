df195c4e092e41c22f05b1df90c22ae9ba3891b5
Pillow <m.farid.ft20@mail.umy.ac.id>
2025-06-05 17:50:20 +0700

    menambah perulangan
    perulangan berisi menu yang berhubungan dengan stack yaitu
    
    1. input value
    2. menghapus elemen teratas
    3. menampilkan elemen teratas tanpa dihapus
    4. menutup program
    
    program akan looping sampai user memilih angka 4

--- stacklinklist.cpp
+++ stacklinklist.cpp
@@ -89,5 +89,36 @@ int main()
     int choice = 0;
     int value;
 
-    
+    while (choice != 4)
+    {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push:";
+            cin >> value;
+            stack.push(value); // Push the entered value onto the stack
+            break;
+        case 2:
+            stack.pop(); // pop the topmost elemetn from the stack
+            break;
+        case 3:
+            stack.peek();
+            break;
+        case 4:
+            cout << "Existing program." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
+        }
+        cout << endl;
+    }
+
+    return 0;
 };
